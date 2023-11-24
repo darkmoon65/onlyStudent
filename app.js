8a4f9a24e0c9d6e2f2cda7807b300986d0abdfad
Cesar Alonso Santillana Q <csantillanaq@ulasalle.edu.pe>
2023-11-24 14:34:05 -0500

    sockets mensajes

--- app.js
+++ app.js
@@ -6,12 +6,51 @@ import authRouter from "./routes/auth.js";
 import userRouter from "./routes/users.js";
 import publicationRouter from "./routes/publication.js";
 import groupRouter from "./routes/group.js";
-
+import { Server } from "socket.io";
+import http from "http";
 
 dotenv.config()
 const app = express();
 app.use(json());
 app.use(cors());
+
+
+// Crear un canal de chats 
+const server = http.createServer(app);
+var io = new Server(
+  {
+    cors: {
+      origin: "http://localhost:3000"
+    }
+  });
+
+io.listen(4000);
+io.on('connection', (socket) => {
+  console.log('Se conectó un usuario');
+  socket.on('chat', (msg) => {
+    io.emit('chat', msg);
+    console.log('mensaje: ' + msg);
+  });
+});
+
+// server.listen(4000, () => {
+//   console.log('listening on *:4000');
+// });
+
+// const chatNamespace = chat.of('/chat');
+// chatNamespace.on('connection', (socket) => {
+//   console.log('Usuario conectado');
+
+//   socket.on('chat message', (msg) => {
+//     console.log('Mensaje en el chat: ' + msg);
+//     chatNamespace.emit('chat message', msg);
+//   });
+
+//   socket.on('disconnect', () => {
+//     console.log('Usuario desconectado del chat');
+//   });
+// });
+
 
 const start = async () => {
   try {
--- controllers/groupController.js
+++ controllers/groupController.js
@@ -3,106 +3,106 @@ import {Grupo} from '../models/groupModel.js'
 
 
 class groupController {
-   static async createGroup (req, res){
-       const dataGroup = new Grupo ({...req.body});
-       const groupInsert = await dataGroup.save();
-       return res.status(201).json(groupInsert);
-   }
+  static async createGroup (req, res){
+      const dataGroup = new Grupo ({...req.body});
+      const groupInsert = await dataGroup.save();
+      return res.status(201).json(groupInsert);
+  }
 
-   static async getAllGroups(req, res) {
-        const token = req.headers.authorization.split(' ')[1]
-        if (!token) return res.status(401).json({ error: 'No se ha enviado el token' })
+  static async getAllGroups(req, res) {
+      const token = req.headers.authorization.split(' ')[1]
+      if (!token) return res.status(401).json({ error: 'No se ha enviado el token' })
 
-        try {
-            jwt.verify(token, process.env.SECRET, async (err) => {
-                if (err) return res.status(403).json({ error: 'Token inválido' })
-            })
-
-            const groups = await Grupo.find();
-            return res.status(200).json(groups);
-
-        } catch (error) {
-            return res.status(500).json({ error: "Error al obtener los grupos" });
-        }
-    }
-
-    static async getGroup(req, res) {
-        const token = req.headers.authorization.split(' ')[1];
-    
-        if (!token) return res.status(401).json({ error: 'No se ha enviado el token' });
-    
-        try {
+      try {
           jwt.verify(token, process.env.SECRET, async (err) => {
-            if (err) return res.status(403).json({ error: 'Token inválido' });
-          });
-    
-          const group = await Grupo.findById(req.params.id);
-          return res.status(200).json(group);
-        } catch (error) {
-          return res.status(500).json({ error: 'Error al obtener el grupo' });
-        }
-      }
+              if (err) return res.status(403).json({ error: 'Token inválido' })
+          })
 
-    static async getGroupsByName(req, res) {
-        const nombre = req.params.nombre;
-      
-        if (!nombre) {
-          return res.status(400).json({ error: "Se debe proporcionar un nombre" });
-        }
-      
-        try {
-          const groups = await Grupo.find({ nombre: nombre });
-      
-          if (groups.length === 0) {
-            return res.status(404).json({ error: "No se encontraron grupos" });
-          }
-      
+          const groups = await Grupo.find();
           return res.status(200).json(groups);
-        } catch (error) {
-          res.status(500).json({ error: "Error al buscar grupos" });
-        }
+
+      } catch (error) {
+          return res.status(500).json({ error: "Error al obtener los grupos" });
+      }
+  }
+
+  static async getGroup(req, res) {
+      const token = req.headers.authorization.split(' ')[1];
+  
+      if (!token) return res.status(401).json({ error: 'No se ha enviado el token' });
+  
+      try {
+        jwt.verify(token, process.env.SECRET, async (err) => {
+          if (err) return res.status(403).json({ error: 'Token inválido' });
+        });
+  
+        const group = await Grupo.findById(req.params.id);
+        return res.status(200).json(group);
+      } catch (error) {
+        return res.status(500).json({ error: 'Error al obtener el grupo' });
+      }
+  }
+
+  static async getGroupsByName(req, res) {
+      const nombre = req.params.nombre;
+    
+      if (!nombre) {
+        return res.status(400).json({ error: "Se debe proporcionar un nombre" });
       }
     
-      static async deleteGroup(req, res) {
-        const token = req.headers.authorization.split(' ')[1];
-        const { id } = req.params;
+      try {
+        const groups = await Grupo.find({ nombre: nombre });
     
-        if (!token) return res.status(401).json({ error: 'No se ha enviado el token' });
-    
-        try {
-          jwt.verify(token, process.env.SECRET, async (err) => {
-            if (err) return res.status(403).json({ error: 'Token inválido' });
-          });
-    
-          const grupoEliminado = await Grupo.findByIdAndDelete(id);
-    
-          return res.status(200).json(grupoEliminado);
-        } catch (error) {
-          return res.status(500).json({ error: 'Error al eliminar el grupo' });
+        if (groups.length === 0) {
+          return res.status(404).json({ error: "No se encontraron grupos" });
         }
+    
+        return res.status(200).json(groups);
+      } catch (error) {
+        res.status(500).json({ error: "Error al buscar grupos" });
       }
+  }
     
-      static async updateGroup(req, res) {
-        const token = req.headers.authorization.split(' ')[1];
-        const { id } = req.params;
-        const updatedGroupData = req.body;
-    
-        if (!token) return res.status(401).json({ error: 'No se ha enviado el token' });
-    
-        try {
-          jwt.verify(token, process.env.SECRET, async (err) => {
-            if (err) return res.status(403).json({ error: 'Token inválido' });
-    
-            await Grupo.findByIdAndUpdate(id, updatedGroupData, { new: true });
-          });
-    
-          const updatedGroup = await Grupo.findById(id);
-    
-          return res.status(200).json(updatedGroup);
-        } catch (error) {
-          return res.status(500).json({ error: 'Error al actualizar el grupo' });
-        }
-      }
+  static async deleteGroup(req, res) {
+    const token = req.headers.authorization.split(' ')[1];
+    const { id } = req.params;
+
+    if (!token) return res.status(401).json({ error: 'No se ha enviado el token' });
+
+    try {
+      jwt.verify(token, process.env.SECRET, async (err) => {
+        if (err) return res.status(403).json({ error: 'Token inválido' });
+      });
+
+      const grupoEliminado = await Grupo.findByIdAndDelete(id);
+
+      return res.status(200).json(grupoEliminado);
+    } catch (error) {
+      return res.status(500).json({ error: 'Error al eliminar el grupo' });
+    }
+  }
+
+  static async updateGroup(req, res) {
+    const token = req.headers.authorization.split(' ')[1];
+    const { id } = req.params;
+    const updatedGroupData = req.body;
+
+    if (!token) return res.status(401).json({ error: 'No se ha enviado el token' });
+
+    try {
+      jwt.verify(token, process.env.SECRET, async (err) => {
+        if (err) return res.status(403).json({ error: 'Token inválido' });
+
+        await Grupo.findByIdAndUpdate(id, updatedGroupData, { new: true });
+      });
+
+      const updatedGroup = await Grupo.findById(id);
+
+      return res.status(200).json(updatedGroup);
+    } catch (error) {
+      return res.status(500).json({ error: 'Error al actualizar el grupo' });
+    }
+  }
 
 }
 
